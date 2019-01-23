ccc8d4858e1ab37a9a32bdc6624d61f93fb3d642
Andrea George <aageorge17@gmail.com>
2019-01-23 23:29:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet){
+  
+if (feet <=400)  
+  return 'This one is on me!'; 
+  if (feet >2500)
+  return 'No can do.';
+if (feet >=2000)
+  return 'I will gladly take your thirty bucks.';
+
+}
+function ternaryCheckCity (city) {
+ return  (city==="NYC") ? 'Ok, sounds good.': 'No go.';
+}
+
+function switchOnCharmFromTip(response) {
+ switch (response) { 
+   case "generous":
+        response ="Thank you so much.";
+        break;
+    case'not as generous':
+        response = 'Thank you.';
+        break;
+    case'thanks for everything':
+        response = 'Bye';
+      }
+
+}
